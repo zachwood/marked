d940ebf6b13b74f9feab35c9e2206d9496a5b7bb
Geof <rectangular@gmail.com>
2011-11-27 20:55:06 -0500

    settttttings and some css tweaks

--- settings.py
+++ settings.py
@@ -3,29 +3,7 @@
 import os, socket
 
 if socket.gethostname() == 'web166.webfaction.com':
-    DEBUG = True
-    
-    # Absolute path to the directory that holds media.
-    # Example: "/home/media/media.lawrence.com/"
-    MEDIA_ROOT = '/home/rectangular/webapps/marked_static/'
-    
-    TEMPLATE_DIRS = (
-        # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-        # Always use forward slashes, even on Windows.
-        # Don't forget to use absolute paths, not relative paths.
-        '/home/rectangular/webapps/marked_django/marked/templates'
-    )
-    
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'rectangular_ma',                      # Or path to database file if using sqlite3.
-            'USER': 'rectangular_ma',                      # Not used with sqlite3.
-            'PASSWORD': '7bc2cfab',                  # Not used with sqlite3.
-            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        }
-    }
+    import settings_deploy.py
 
 else:
     DEBUG = True
